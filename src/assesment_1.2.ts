0f79b3bff3c58d7684f0b04c3510aeb459934a75
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-03-20 17:04:21 +0530

    recent commit

--- src/assesment_1.2.ts
+++ src/assesment_1.2.ts
@@ -1,5 +1,5 @@
 interface ISavingAccount{
-   balance:number
+    balance:number
     Balance:()=>void
 }
 interface ICurrentAccount{
@@ -7,20 +7,18 @@ interface ICurrentAccount{
     Balance:()=>void
 }
 export class Account implements ICurrentAccount,ISavingAccount {
-
-balance: number;
+ balance: number;
  account_type:number;
      
     constructor(account_type:number,balance: number){
        this.balance=balance;
+
        this.account_type=account_type
     }
     Balance(): void{
-
-
       if(this.account_type==1){
      console.log(`Saving Balance:${this.balance}`);}
-     else if(this.account_type==2)
+     else if(this.account_type == 2)
      { console.log(`Currrent Balance:${this.balance}`)}
    }
 
